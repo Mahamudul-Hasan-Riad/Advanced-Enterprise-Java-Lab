58f982b5313aa525770c0e855ab827cc58b94f9c
Mahamudul-Hasan-Riad <46709306+Mahamudul-Hasan-Riad@users.noreply.github.com>
2019-08-21 22:21:13 +0600

    Work lab_01
    java

---
+++ lab_01/Datatype.java
@@ -0,0 +1,20 @@
+package lab_01;
+
+public class Datatype {
+	public static void main(String[]args) {
+		
+		String name = "Md Riad Mia";
+		long id = 151382337;
+		int batch = 38;
+		String bg = "O positive";
+		 
+		System.out.println("Name: "+ name);
+		System.out.println("ID: "+id);
+		System.out.println("Batch: "+batch);
+		System.out.println("Blood Group: "+bg);
+
+
+		
+	}
+
+}
